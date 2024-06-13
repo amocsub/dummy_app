0a40284a808d75a56a73eea480fb47c1513f26da
Matias Busco <18237644+amocsub@users.noreply.github.com>
2024-06-13 17:52:33 +0200

    fourth commit

--- __main__.py
+++ __main__.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 from dummy_submodule import greeting
+from dummy_subtree import greeting_tree
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     """Handles the root URL and returns 'Hello, World!'."""
-    return greeting(request.args.get('name'))
+    return greeting(f"{greeting_tree()} {request.args.get('name')}")
 
 if __name__ == '__main__':
     app.run(debug=True)
